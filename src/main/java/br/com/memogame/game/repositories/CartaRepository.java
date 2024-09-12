bc8db39f5deb833bb620d8015202bab4074792c9
Thomas Pinheiro <thomashenriquesantospinheiro@gmail.com>
2024-09-11 22:06:19 -0300

    refactor: Ranking.java
    Campo usuario_id estabelecido como não-nulo.

--- src/main/java/br/com/memogame/game/repositories/CartaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.memogame.game.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import br.com.memogame.game.models.Carta;
-
-
-public interface CartaRepository extends CrudRepository<Carta, Integer> {
-
-}
--- src/main/java/br/com/memogame/game/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.memogame.game.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import br.com.memogame.game.models.Usuario;
-import br.com.memogame.game.models.Ranking;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Integer>{
-
-}
