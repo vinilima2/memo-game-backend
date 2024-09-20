db665acb8eecebfaee04a4f3e4431bac63b1910f
Thomas Pinheiro <thomashenriquesantospinheiro@gmail.com>
2024-09-19 23:04:09 -0300

    refactor: Teste.java
    Desativação dos testes de criação de usuário e ranking (comentado) para suprimir warnings.

--- src/test/java/br/com/memogame/game/Teste.java
+++ src/test/java/br/com/memogame/game/Teste.java
@@ -3,31 +3,31 @@ package br.com.memogame.game;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-import br.com.memogame.game.repositories.*;
-import br.com.memogame.game.models.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//import br.com.memogame.game.repositories.*;
+//import br.com.memogame.game.models.*;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Teste {
-    @Autowired
-    private UsuarioRepo ur;
+    //@Autowired
+    //private UsuarioRepo ur;
 
-    @Autowired
-    private RankingRepo rr;
+    //@Autowired
+    //private RankingRepo rr;
 
     //@Autowired
     //private CartaRepo cr;
 
     @Before
     public void setUp() throws Exception {
-        List<Usuario> usuarios = new ArrayList<Usuario>();
+        /*List<Usuario> usuarios = new ArrayList<Usuario>();
         List<Ranking> rankings = new ArrayList<Ranking>();
         Usuario u1 = new Usuario("Thomas", "thomas@gmail.com", "hioudfvhioufgs"), u2 = new Usuario("Diego", "lesmarill@hotmail.com", "jadhshuads8123&&das(*)"), u3 = new Usuario("Vinicius", "vinilima81@yahoo.com.br", "adfisuyer234y789"), u4 = new Usuario("Mateus", "mateuzin67@uol.com.br", "1235467fgmnfghj**C&&*V"), u5 = new Usuario("Eduardo", "educomercial@telecom.com.br", "89r234huy7defq7cefr");
         Ranking r1 = new Ranking(90123l), r2 = new Ranking(89123l), r3 = new Ranking(39012391l), r4 = new Ranking(19299l), r5 = new Ranking(100l);
@@ -42,7 +42,8 @@ public class Teste {
         r2.setUsuario(u2);
         r1.setUsuario(u1);
         ur.save(u1); ur.save(u2); ur.save(u3); ur.save(u4); ur.save(u5);
-        rr.save(r1); rr.save(r2); rr.save(r3); rr.save(r4); rr.save(r5);
+        rr.save(r1); rr.save(r2); rr.save(r3); rr.save(r4); rr.save(r5); 
+        */
         
         /*List<Carta> cartas = new ArrayList<Carta>();
         cartas.add(new Carta("estrela", "amarelo"));
