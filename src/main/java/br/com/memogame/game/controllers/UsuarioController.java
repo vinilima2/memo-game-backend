2407a1fce1e01588513b6a470969ae0ae206e370
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2024-09-25 19:10:36 -0300

    refactor: Retorna o token no body do response ao realizar login

--- src/main/java/br/com/memogame/game/controllers/UsuarioController.java
+++ src/main/java/br/com/memogame/game/controllers/UsuarioController.java
@@ -1,5 +1,6 @@
 package br.com.memogame.game.controllers;
 
+import br.com.memogame.game.dtos.*;
 import br.com.memogame.game.services.AuthService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,16 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.memogame.game.repositories.RankingRepo;
 import br.com.memogame.game.repositories.UsuarioRepo;
-import br.com.memogame.game.dtos.UsuarioDto;
-import br.com.memogame.game.dtos.UsuarioLoginDto;
-import br.com.memogame.game.dtos.UsuarioPontuacaoDto;
-import br.com.memogame.game.dtos.UsuarioCadastroDto; 
 import br.com.memogame.game.models.Usuario;
 import br.com.memogame.game.models.Ranking;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
 
 @RestController
 @RequestMapping("usuarios") // --> localhost:8080/usuarios
@@ -35,7 +30,7 @@ public class UsuarioController {
 
     @PostMapping("/login") // --> localhost:8080/usuarios/login # POST
     @ResponseBody
-    public UsuarioDto login(@RequestBody UsuarioLoginDto login, HttpServletResponse response) {
+    public LoginResponseDto login(@RequestBody UsuarioLoginDto login, HttpServletResponse response) {
         Usuario usuario = repo.findByNome(login.nome());
 
         if (usuario == null || !usuario.getSenha().equals(login.senha())) throw new RuntimeException("Usuário ou senha inválidos");
@@ -47,7 +42,7 @@ public class UsuarioController {
         cookie.setPath("/");
         response.addCookie(cookie);
 
-        return new UsuarioDto(usuario);
+        return new LoginResponseDto(token);
     }
     @PostMapping("/cadastrar") // --> localhost:8080/usuarios/cadastrar # POST
     @ResponseBody
